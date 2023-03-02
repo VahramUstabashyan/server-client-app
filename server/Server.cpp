09fcb631f40f51b8d342cfcf8e9e4101e75388e4
VahramUstabashyan <ustabashyanv@gmail.com>
2023-03-02 23:48:49 +0400

    Implement `Server::shell` method

--- server/Server.cpp
+++ server/Server.cpp
@@ -53,3 +53,18 @@ void Server::handle_connection(boost::system::error_code err,
 std::string Server::handle_new_message(std::string msg) {
     return shell(msg);
 }
+
+std::string Server::shell(const std::string& command) {
+    std::array<char, 128> buffer{};
+    std::string output;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(
+            popen(command.c_str(), "r"),
+            pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        output += buffer.data();
+    }
+    return output;
+}
