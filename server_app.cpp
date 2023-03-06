2cb82f8e5818729b234638828b3bba6f53401241
VahramUstabashyan <ustabashyanv@gmail.com>
2023-03-06 12:08:34 +0400

    Add argument passing to the server application

--- server_app.cpp
+++ server_app.cpp
@@ -5,11 +5,25 @@
 #include "server/Server.hpp"
 
 
-int main() {
+int main(int argc, char** argv) {
+   std::string ip = "0.0.0.0";
+   int port = 55555;
+
+   if (argc == 3) {
+      ip = argv[1];
+      port = std::stoi(argv[2]);
+   } else if (argc == 1) {
+      std::cout << "IP and port set to " << ip << " and " << port << " respectively." << std::endl;
+   } else {
+      std::cout << "Usage: " << argv[0] << std::endl;
+      std::cout << "   OR: " << argv[0] << " <IP> <PORT>" << std::endl;
+      exit(0);
+   }
+
     std::cout << "This is server application." << std::endl;
     std::cout << "Enter 'quit' to stop." << std::endl;
 
-    auto server = std::make_shared<Server>("0.0.0.0", 8080);
+    auto server = std::make_shared<Server>(ip, port);
 
     bool quit = false;
     while (!quit) {
