a02ab5984f14ed1f9fdce1b0211394c31a5e7d70
Vahram Ustabashyan <ustabashyanv@gmail.com>
2023-03-19 01:16:29 +0400

    Use unordered map in command handling and add `help`

--- client/ClientCommandHandler.cpp
+++ client/ClientCommandHandler.cpp
@@ -6,30 +6,24 @@ ClientCommandHandler::ClientCommandHandler(std::shared_ptr<Client> client)
 
 bool ClientCommandHandler::handle_command(const std::string& command) {
     if (command.empty()) return false;
-    try {
-        std::istringstream command_ss(command);
-        std::string token = get_argument(command_ss);
-        if (token == "connect") {
-            /// Handle connect
-                std::string ip = get_argument(command_ss);
-                int port = std::stoi(get_argument(command_ss));
-                client->connect(ip, port);
-        } else if (token == "disconnect") {
-            /// Handle disconnect
-            client->disconnect();
-        } else if (token == "shell") {
-            /// Handle shell command
-            std::string cmd;
-            cmd = get_in_quotes(command_ss);
-            client->shell(cmd);
-        } else if (token == "quit") {
-            /// Handle quit
-            return true;
-        } else {
-            std::cerr << "Unknown command: " << token << std::endl;
+
+    std::istringstream command_ss(command);
+    std::string token = get_argument(command_ss);
+
+    if (token == "quit") {
+        check_empty(command_ss);
+        return true;
+    }
+
+    auto cmd = commands.find(token);
+    if (cmd != commands.end()) {
+        try {
+            (this->*(cmd->second))(command_ss);
+        } catch (std::invalid_argument& e) {
+            std::cerr << "Error while processing command: " << e.what() << std::endl;
         }
-    } catch (std::invalid_argument& e) {
-        std::cerr << "Error while processing command: " << e.what() << std::endl;
+    } else {
+        std::cerr << "Unknown command: " << token << std::endl;
     }
     return false;
 }
@@ -53,4 +47,43 @@ std::string ClientCommandHandler::get_in_quotes(std::istringstream& ss) {
         throw std::invalid_argument("Missing quote(s)");
     }
     return token;
+}
+
+void ClientCommandHandler::check_empty(std::istringstream& ss) {
+    std::string token;
+    std::getline(ss, token, ' ');
+    if (!token.empty()) {
+        throw std::invalid_argument("Unknown argument: " + token);
+    }
+}
+
+void ClientCommandHandler::connect(std::istringstream& ss) {
+    std::string ip = get_argument(ss);
+    int port = std::stoi(get_argument(ss));
+    check_empty(ss);
+    client->connect(ip, port);
+}
+
+void ClientCommandHandler::disconnect(std::istringstream& ss) {
+    check_empty(ss);
+    client->disconnect();
+}
+
+void ClientCommandHandler::shell(std::istringstream& ss) {
+    std::string cmd;
+    cmd = get_in_quotes(ss);
+    check_empty(ss);
+    client->shell(cmd);
+}
+
+void ClientCommandHandler::help(std::istringstream& ss) {
+    check_empty(ss);
+    std::cout << "Available commands:" << std::endl;
+    for (auto cmd : commands) {
+        std::cout << " - " << cmd.first << std::endl;
+    }
+}
+
+void ClientCommandHandler::quit(std::istringstream& ss) {
+    check_empty(ss);
 }
--- client/ClientCommandHandler.hpp
+++ client/ClientCommandHandler.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <string>
 #include <memory>
+#include <unordered_map>
 
 #include "Client.hpp"
 
@@ -11,6 +12,16 @@ class ClientCommandHandler {
 private:
     /// Client to handle commands for
     std::shared_ptr<Client> client;
+
+    /// Available commands map
+    typedef void (ClientCommandHandler::*command_method_t)(std::istringstream&);
+    const std::unordered_map<std::string, command_method_t> commands = {
+            {"quit", &ClientCommandHandler::quit},
+            {"help", &ClientCommandHandler::help},
+            {"shell", &ClientCommandHandler::shell},
+            {"disconnect", &ClientCommandHandler::disconnect},
+            {"connect", &ClientCommandHandler::connect}
+    };
 public:
     /// Constructor
     explicit ClientCommandHandler(std::shared_ptr<Client> client);
@@ -36,4 +47,40 @@ private:
      * @return Value in quotes
      */
     static std::string get_in_quotes(std::istringstream& ss);
+
+    /**
+     * Check if no more arguments left and throw otherwise
+     * @param ss Input sstream
+     */
+    static void check_empty(std::istringstream& ss);
+
+    /**
+     * Connect to the server
+     * @param ss Input sstream
+     */
+    void connect(std::istringstream& ss);
+
+    /**
+     * Disconnect from the server
+     * @param ss Input sstream
+     */
+    void disconnect(std::istringstream& ss);
+
+    /**
+     * Run shell command on the server
+     * @param ss Input sstream
+     */
+    void shell(std::istringstream& ss);
+
+    /**
+     * Show available command list
+     * @param ss Input sstream
+     */
+    void help(std::istringstream& ss);
+
+    /**
+     * Quit the application
+     * @param ss Input sstream
+     */
+    void quit(std::istringstream& ss);
 };
